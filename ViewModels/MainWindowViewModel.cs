64a9182b4edde95cce75e6dd56f03ff0a6efca4f
Федоров Евгений Андреевич <Viva_la_lumiere@mail.ru>
2021-10-14 10:40:46 +0700

    Добавьте файлы проекта.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Microsoft.Win32;
+using VideoProcessingXRay.Commands;
+using VideoProcessingXRay.Commands.Base;
+
+namespace VideoProcessingXRay.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private double variable;
+        public double Variable
+        {
+            get => variable;
+            set => Set(ref variable, value);
+        }
+
+        private string _titile = "Video Processing";
+        public string Title
+        {
+            get => _titile;
+            set => Set(ref _titile, value);
+        }
+
+
+        public ICommand EmptyCommand { get; }
+        private void OnEmptyCommandExecuted(object p)
+        {
+           
+
+        }
+        private bool CanEmptyCommandExecute(object p) => true;
+
+        public MainWindowViewModel()
+        {
+
+            EmptyCommand = new LambdaCommand(OnEmptyCommandExecuted, CanEmptyCommandExecute);
+
+            this.PropertyChanged += MainWindowViewModel_PropertyChanged;
+        }
+
+        private void MainWindowViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+
+        }
+
+    }
+
+}
---
+++ ViewModels/ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace VideoProcessingXRay.ViewModels
+{
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        private bool _Disposed;
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+        }
+    }
+}
