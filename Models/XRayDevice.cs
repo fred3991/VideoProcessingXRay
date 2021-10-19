3252560a0424be55474f1a4b88a989da486ee777
Федоров Евгений Андреевич <Viva_la_lumiere@mail.ru>
2021-10-19 08:21:13 +0700

    Timer

---
+++ Models/XRayDevice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace VideoProcessingXRay.Models
+{/// <summary>
+/// генерирует пачку изображений, которую мы потом будем использовать
+/// </summary>
+    public class XRayDevice
+    {
+        public List<string> ImagesSet = new List<string>();
+
+        public XRayDevice()
+        {
+            GenerateImagesDB();
+        }
+
+        public void GenerateImagesDB()
+        {
+            int fileCount = Directory.GetFiles(@"C:\Users\Viva_\Source\Repos\VideoProcessingXRay\ImageDB").Length;
+            for (int i=1; i< fileCount+1; i++)
+            {
+                string filePath = @"C:\Users\Viva_\Source\Repos\VideoProcessingXRay\ImageDB\"+i+".jpg";
+                ImagesSet.Add(filePath);
+            }       
+        }
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,41 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
+
 using System.Windows.Input;
-using System.Xml;
-using System.Xml.Linq;
-using System.Xml.Serialization;
-using Microsoft.Win32;
-using VideoProcessingXRay.Commands;
+using System.Windows.Threading;
+
 using VideoProcessingXRay.Commands.Base;
+using VideoProcessingXRay.Models;
 
 namespace VideoProcessingXRay.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
-        private double variable;
-        public double Variable
-        {
-            get => variable;
-            set => Set(ref variable, value);
-        }
-
+     
         private string _titile = "Video Processing";
         public string Title
         {
             get => _titile;
             set => Set(ref _titile, value);
         }
+
+        public XRayDevice xrDevice; 
+
+        private string _activeFrame = @"C:\Users\Viva_\Source\Repos\VideoProcessingXRay\ImageDB\1.jpg";
+        public string ActiveFrame
+        {
+            get => _activeFrame;
+            set => Set(ref _activeFrame, value);
+        }
+
+        private int _startFrameNum = 1;
+        public int StartFrameNum
+        {
+            get => _startFrameNum;
+            set => Set(ref _startFrameNum, value);
+        }
+
+        private int _stopFrameNum = 30;
+        public int StopFrameNum
+        {
+            get => _stopFrameNum;
+            set => Set(ref _stopFrameNum, value);
+        }
+
+        private int _activeFrameNum = 1;
+        public int ActiveFrameNum
+        {
+            get => _activeFrameNum;
+            set => Set(ref _activeFrameNum, value);
+        }
+
+
+
+        private int _frameNum = 1;
+        public int FrameNum
+        {
+            get => _frameNum;
+            set => Set(ref _frameNum, value);
+        }
+
+        //private Image _currentImage = Image.FromFile
+        ////public Image CurrentImage
+        ////{
+        ////    get => _currentImage;
+        ////    set => Set(ref _currentImage, value);
+        ////}
+
+
+
 
         #region Task1
 
@@ -64,38 +94,120 @@ namespace VideoProcessingXRay.ViewModels
         public ICommand TaskOneGenerateCommand { get; }
         private void OnTaskOneGenerateCommandExecuted(object p)
         {
-            // Здесь обработка задания 1
+            DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.Render);
+
+            var frameRate = 1 / FrameNum;
+            timer.Interval = TimeSpan.FromSeconds(0.1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+
+
+            //var inputFile = @"C:\Users\Viva_\Source\Repos\VideoProcessingXRay\ImageDB\1.jpg";
+            //var binInputFile = File.ReadAllBytes(inputFile);
+            //var memInput = new MemoryStream(binInputFile);
+            //var bitmap = Bitmap.FromStream(memInput) as Bitmap;
+
+
+
         }
         private bool CanTaskOneGenerateCommandExecute(object p) => true;
-
-
-
-
         #endregion
 
 
         public ICommand EmptyCommand { get; }
         private void OnEmptyCommandExecuted(object p)
         {
-           
+
 
         }
         private bool CanEmptyCommandExecute(object p) => true;
 
         public MainWindowViewModel()
         {
-
+            xrDevice = new XRayDevice();
             EmptyCommand = new LambdaCommand(OnEmptyCommandExecuted, CanEmptyCommandExecute);
             TaskOneGenerateCommand = new LambdaCommand(OnTaskOneGenerateCommandExecuted, CanTaskOneGenerateCommandExecute);
 
-            this.PropertyChanged += MainWindowViewModel_PropertyChanged;
+
         }
 
-        private void MainWindowViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+
+
+        public void TimerThread()
         {
+           
 
         }
 
-    }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (ActiveFrameNum < StopFrameNum)
+            {
+                ActiveFrame = @"c:\users\viva_\source\repos\videoprocessingxray\imagedb\" + ActiveFrameNum + ".jpg";
+                ActiveFrameNum++;
+            }
+            else
+            {
+                ActiveFrameNum = 1;
+            }
+                     
+        }
 
+        //public void ImageSequenceThread()
+        //{
+        //    Thread ImageThread = new Thread(new ThreadStart(ImageSequence));
+        //    ImageThread.Start();
+        //    ImageThread.Priority = ThreadPriority.Highest;
+        //}
+        //private void TimerTick(object sender, EventArgs e)
+        //{
+        //    ActiveFrame = @"c:\users\viva_\source\repos\videoprocessingxray\imagedb\" + n + ".jpg";
+
+        //    if (++imageIndex == 5)
+        //    {
+        //        imageIndex = 2;
+        //    }
+        //}
+
+  
+        public void SetFrameImage(object x)
+        {           
+            int n = (int)x;
+            FrameNum = n;
+            ActiveFrame = @"c:\users\viva_\source\repos\videoprocessingxray\imagedb\" + n + ".jpg";
+
+        }
+
+        ///// <summary>
+        ///// Resize the image to the specified width and height.
+        ///// </summary>
+        ///// <param name="image">The image to resize.</param>
+        ///// <param name="width">The width to resize to.</param>
+        ///// <param name="height">The height to resize to.</param>
+        ///// <returns>The resized image.</returns>
+        //public static Image ResizeImage(Image image, int width, int height)
+        //{
+        //    var destRect = new Rectangle(0, 0, width, height);
+        //    var destImage = new Bitmap(width, height);
+
+        //    destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+        //    using (var graphics = Graphics.FromImage(destImage))
+        //    {
+        //        graphics.CompositingMode = CompositingMode.SourceCopy;
+        //        graphics.CompositingQuality = CompositingQuality.HighQuality;
+        //        graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+        //        graphics.SmoothingMode = SmoothingMode.HighQuality;
+        //        graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+        //        using (var wrapMode = new ImageAttributes())
+        //        {
+        //            wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+        //            graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+        //        }
+        //    }
+
+        //    return destImage;
+        //}
+    }
 }
